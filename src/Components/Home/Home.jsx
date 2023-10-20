04a85be270d6ccc1c502f1a2ac5df84396760481
Lalit Jakhar <lalit25jakhar@email.com>
2023-10-20 18:01:09 +0530

    Initial commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,36 +11,35 @@ const Home = () => {
         console.log(res);
       });
   }, []);
+
   return (
     <>
-      <Container>
-        <Box sx={{ marginBlock: "2rem" }}>
-          <Grid container spacing={2}>
-            {data?.products?.map((mobile, index) => {
-              return (
-                <Grid item xs>
-                  <img src={mobile.thumbnail} alt="" width={350} height={400} />
-                  <Box>
-                    <Box sx={{ display: "flex", justifyContent: "center" }}>
-                      <Typography variant="h5" sx={{ fontWeight: "600" }}>
-                        {mobile.title}
-                      </Typography>
-                    </Box>
-                    <Box sx={{ display: "flex", justifyContent: "center" }}>
-                      <Typography variant="h6">{mobile.brand}</Typography>
-                    </Box>
-                    <Box sx={{ display: "flex", justifyContent: "center" }}>
-                      <Typography sx={{ color: "gray" }}>
-                        {mobile.price}
-                      </Typography>
-                    </Box>
+      <Box sx={{ margin: "2rem" }}>
+        <Grid container spacing={2}>
+          {data?.products?.map((mobile, index) => {
+            return (
+              <Grid item xs>
+                <img src={mobile.thumbnail} alt="" width={350} height={400} />
+                <Box>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Typography variant="h5" sx={{ fontWeight: "600" }}>
+                      {mobile.title}
+                    </Typography>
                   </Box>
-                </Grid>
-              );
-            })}
-          </Grid>
-        </Box>
-      </Container>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Typography variant="h6">{mobile.category}</Typography>
+                  </Box>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Typography sx={{ color: "gray" }}>
+                      {mobile.description}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Grid>
+            );s
+          })}
+        </Grid>
+      </Box>
     </>
   );
 };
